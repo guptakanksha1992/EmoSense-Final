a421962827c3ced868faadbfb4defc49f41fc065
guptakanksha1992 <guptakanksha1992@gmail.com>
2017-04-03 10:37:57 -0400

    Files pushed

---
+++ ElasticSearchServices.py
@@ -0,0 +1,53 @@
+# This is the file where we connect to ES + find and insert data
+
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+
+# Our elastic search engine
+#awsauth = AWS4Auth('AKIAINYR7GOTUOL4IWUQ', 'E4yhGRR4QBGlLKLMAy/DEpYy5zqq9QMN54uN050q', "us-west-1", 'es')
+HOSTADDRESS='search-tweetmap-whpei25apwtxe7bvkmaj34ozee.us-west-1.es.amazonaws.com'
+#awsauth = AWS4Auth('AKIAINHSJW74HQTPGTYQ', 'ROcOAC4P3iJ4pkX1ySJjDNJfvTzmLFFqAcE7XW3l', "us-west-1", 'es')
+awsauth = AWS4Auth('AKIAINYR7GOTUOL4IWUQ', 'E4yhGRR4QBGlLKLMAy/DEpYy5zqq9QMN54uN050q', "us-west-1", 'es')
+
+class ElasticSearchServices:
+
+    def __init__(self):
+        self.es = Elasticsearch(
+            hosts=[{'host': HOSTADDRESS, 'port': 443}],
+            http_auth=awsauth,
+            use_ssl=True,
+            verify_certs=True,
+            connection_class=RequestsHttpConnection
+        )
+
+    def store_data(self, index, doc_type, body):
+        results = self.es.index(
+    			index=index,
+    			doc_type=doc_type,
+    			body=body
+    		)
+
+        return results
+
+    def create_collection(self, index, body):
+        print ("Creating collection...")
+        results = self.es.indices.create(
+            index=index,
+            ignore=400,
+            body=body
+        )
+        return results
+
+    def search(self, index, doc_type, body, size):
+    	results = self.es.search(
+    			index = index,
+    			doc_type = doc_type,
+    			body = body,
+    			size = size
+    		)
+
+    	return results
+
+    def total_hits(results):
+    	return results['hits']['total']
+
---
+++ TweetListener.py
@@ -0,0 +1,137 @@
+import tweepy
+import json
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+from TweetHandler import TwitterHandler
+from ElasticSearchServices import ElasticSearchServices
+import random
+
+
+#----------Twitter API Details---------------------------
+
+consumerKey='uJ8ywVGKTC7aubwomDuWrAu9t'
+consumerSecret='qbcDPiGjdNGj3B2EiXja3z0ppxMenePTzp6X1nAur2CakwLF1G'
+accessToken='3287103102-m7iUaz9H6eOmgl50DBMXPfePIywVFEnYldLAUoa'
+accessSecret='lrJiIAcGZRvxPNTTvo5TCe3KRJp6FaqNZGIOC0SSOHLsx'
+
+KEYWORDS = ['Food', 'Travel', 'Hollywood', 'Art', 'Cartoons', 'Pizza', 'Friends', 'Miami']
+REQUEST_LIMIT = 420
+
+#---- Elastic Search Details -------
+
+index = "finaltwittermapindex5"
+collection = {
+	"mappings": {
+		"finaltweets2": {
+			"properties": {
+				"id": {
+					"type": "string"
+				},
+				"message": {
+					"type": "string"
+				},
+				"author": {
+					"type": "string"
+				},
+				"timestamp": {
+					"type": "string"
+				},
+				"location": {
+					"type": "geo_point"
+				}
+			}
+		}
+	}
+}
+
+#--------------------------------------------------------
+
+
+try:
+    collection_service = ElasticSearchServices()
+    collection_service.create_collection(index, collection)
+except:
+    print "Index already created"
+
+class TweetListener(StreamListener):
+
+    def on_data(self, data):
+        try:
+            parse_data(data)
+        except:
+            # print(data)
+            print("No location data found")
+
+        return(True)
+
+    def on_error(self, status):
+        errorMessage = "Error - Status code " + str(status)
+        print(errorMessage)
+        if status == REQUEST_LIMIT:
+            print("Request limit reached. Trying again...")
+            exit()
+
+def parse_data(data):
+    json_data_file = json.loads(data)
+    tweetHandler = TwitterHandler()
+
+    location = json_data_file["place"]
+    coordinates = json_data_file["coordinates"]
+
+    if coordinates is not None:
+        # print(json_data_file["coordinates"])
+        final_longitude = json_data_file["coordinates"][0]
+        final_latitude = json_data_file["coordinates"][0]
+    elif location is not None:
+        coord_array = json_data_file["place"]["bounding_box"]["coordinates"][0]
+        longitude = 0;
+        latitude = 0;
+        for object in coord_array:
+            longitude = longitude + object[0]
+            latitude = latitude + object[1]
+
+
+        final_longitude = longitude / len(coord_array)
+        final_latitude = latitude / len(coord_array)
+    else:
+
+    	# Insert code for random final_longitude, final_latitude here
+
+        final_longitude=random.uniform(-180.0,180.0)
+        final_latitude=random.uniform(-90.0, +90.0)
+        
+    tweetId = json_data_file['id_str']
+    tweet = json_data_file["text"]
+    author = json_data_file["user"]["name"]
+    timestamp = json_data_file["created_at"]
+
+    location_data = [final_longitude, final_latitude]
+    try:
+        print(tweetHandler.insertTweet(tweetId, location_data, tweet, author, timestamp))
+    except:
+        print("Failed to insert tweet")
+
+def startStream():
+
+    auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+    auth.set_access_token(accessToken, accessSecret)
+    while True:
+        try:
+            twitterStream = Stream(auth, TweetListener())
+            twitterStream.filter(track=KEYWORDS)
+        except:
+            print("Restarting Stream")
+            continue
+
+    #The location specified above gets all tweets, we can then filter and store based on what we want
+
+# For testing purposes only
+'''
+if __name__ == '__main__':
+    while True:
+        try:
+            startStream()
+        except:
+            print("Print restart")
+            continue
+'''
